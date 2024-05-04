8459cc96b7686a1f7c64a0254eb61de95b2e4fcb
UASTRONGMAN <shkolniy.maks@ukr.net>
2024-05-04 14:33:58 +0300

    HW2

---
+++ src/Models/PostModel/PostModel.ts
@@ -0,0 +1,8 @@
+export interface PostModel{
+    id: number,
+    title: string,
+    body: string,
+    userId: number,
+    tags: string[],
+    reactions: number
+}
---
+++ src/Models/UserModel/UserModel.ts
@@ -0,0 +1,14 @@
+export interface UserModel {
+    id: number,
+    firstName: string,
+    lastName: string,
+    maidenName: string,
+    age: 50,
+    gender: string,
+    email: string,
+    phone: number,
+    birthDate: number,
+    image: string
+}
+
+// Виводжу не всі поля)
